5dd6ea0911825e242a694bf3a1768aa59ce257ca
Rohit Pandey <rd8614196@gmail.com>
2025-08-30 10:40:58 +0530

    UI issue on mobile fix

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -7,7 +7,7 @@ export default function ProjectCard({title,data,image,link}) {
     <>
         <Col sm={6} md={4}>
             <a href={link} className='project-card-hover-text'>
-            <div className="project-box" style={{width:"420px", height:"330px", position: "relative", overflow: "hidden"}}>
+            <div className="project-box" style={{width: window.innerWidth > 768 ? "420px" : "335px", height:"335px", position: "relative", overflow: "hidden"}}>
                 <img src={image} alt="hello" style={{width : "100%", height : "100%", objectFit: "cover", transformOrigin: "top"}}/>
                 <div className="text">
                     <h4>{title}</h4>
